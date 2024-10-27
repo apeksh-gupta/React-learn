8c673f93b725dd7f2183df5ff4c609f576ef7caf
apeksh-gupta <apekshgupta00@gmail.com>
2024-10-27 23:43:44 +0530

    initialised currency converter

---
+++ currency-converter/src/App.jsx
@@ -0,0 +1,14 @@
+
+
+function App() {
+
+  return (
+    <>
+     <h1 
+      className="text-4xl text-sky-700 bg-blue-100 p-4 font-serif font-bold text-center"
+     >Currency coverter</h1>
+    </>
+  )
+}
+
+export default App
